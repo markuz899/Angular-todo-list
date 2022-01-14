0089c9c7e258ba52cf5e50fcaa08ea7370efa309
markuz899 <markuz.89@hotmail.it>
2022-01-14 11:17:10 +0100

    http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
+import { GitHubService } from './service/github.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodosComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, TodosComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  providers: [GitHubService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,49 +1,78 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from './../../models/Todo';
+import { GitHubService } from 'src/app/service/github.service';
+import { Repos } from '../../models/repos';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+  styleUrls: ['./todos.component.scss'],
 })
-
 export class TodosComponent implements OnInit {
-  todos:Todo[] | undefined;
-  inputTodo:string = "";
+  todos: Todo[] | undefined;
+  inputTodo: string = '';
 
-  constructor() { }
+  userName: string = 'markuz899';
+  repos: Repos[] = [];
+  loading: boolean = false;
+  errorMessage: string = '';
+
+  constructor(private githubService: GitHubService) {}
+
+  getRepos() {
+    this.loading = true;
+    this.errorMessage = '';
+    this.githubService.getRepos(this.userName).subscribe(
+      (response) => {
+        //next() callback
+        console.log('response received');
+        this.repos = response;
+      },
+      (error) => {
+        //error() callback
+        console.error('Request failed with error');
+        this.errorMessage = error;
+        this.loading = false;
+      },
+      () => {
+        //complete() callback
+        console.error('Request completed'); //This is actually not needed
+        this.loading = false;
+      }
+    );
+  }
 
   ngOnInit(): void {
     this.todos = [
       {
-        content:'First todo',
-        completed: false
+        content: 'First todo',
+        completed: false,
       },
       {
-        content:'Second todo',
-        completed: false
-      }
-    ]
+        content: 'Second todo',
+        completed: false,
+      },
+    ];
   }
 
-  toggleDone (id:number) {
-    this.todos?.map((v,i) => {
-      if(i == id) v.completed = !v.completed;
+  toggleDone(id: number) {
+    this.todos?.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
       return v;
-    })
+    });
   }
 
-  deleteTodo (id:number) {
-    this.todos = this.todos?.filter((v,i) => i !== id);
+  deleteTodo(id: number) {
+    this.todos = this.todos?.filter((v, i) => i !== id);
   }
 
-  addTodo () {
-    if(this.inputTodo !== ''){
+  addTodo() {
+    if (this.inputTodo !== '') {
       this.todos?.push({
         content: this.inputTodo,
-        completed: false
-      })
-      this.inputTodo = ''
+        completed: false,
+      });
+      this.inputTodo = '';
     }
   }
 }
